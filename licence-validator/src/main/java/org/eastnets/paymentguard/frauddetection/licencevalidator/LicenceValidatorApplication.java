48cd2ae02acf1c4aabcac1038267e6db0dbe7d68
OmarAzzam <oma20201175@std.psut.edu.jo>
2024-08-08 01:08:55 +0300

    intial comit - implemeted eureka service discovery also i setted up the

---
+++ licence-validator/src/main/java/org/eastnets/paymentguard/frauddetection/licencevalidator/LicenceValidatorApplication.java
@@ -0,0 +1,14 @@
+package org.eastnets.paymentguard.frauddetection.licencevalidator;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class LicenceValidatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LicenceValidatorApplication.class, args);
+    }
+}
