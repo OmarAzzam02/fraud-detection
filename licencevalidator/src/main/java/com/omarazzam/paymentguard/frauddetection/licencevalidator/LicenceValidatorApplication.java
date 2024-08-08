0a9b39fa849756067cebcecef232f8f265f2cd19
OmarAzzam <oma20201175@std.psut.edu.jo>
2024-08-08 10:32:49 +0300

    naming convention

---
+++ licencevalidator/src/main/java/com/omarazzam/paymentguard/frauddetection/licencevalidator/LicenceValidatorApplication.java
@@ -0,0 +1,14 @@
+package com.omarazzam.paymentguard.frauddetection.licencevalidator;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class LicenceValidatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LicenceValidatorApplication.class, args);
+    }
+}
